d6e252f0511d6c0719566393be56eeb1aaf7fcef
jaly <dcdlove@qq.com>
2021-10-08 10:27:49 +0800

    增加难度

--- js/script.js
+++ js/script.js
@@ -121,7 +121,26 @@ new Vue({
       return Math.floor(Math.random() * (max - min)) + min
     },
     init() {
-      (() => { function block() { if ( window.outerHeight - window.innerHeight > 200 || window.outerWidth - window.innerWidth > 200 ) { window.location = "sb.fsfun.cn"; } setInterval(() => { (function () { return false; } ["constructor"]("debugger") ["call"]()); }, 50); } try { block(); } catch (err) {} })();
+      ;(() => {
+        function block() {
+          if (
+            window.outerHeight - window.innerHeight > 200 ||
+            window.outerWidth - window.innerWidth > 200
+          ) {
+            document.body.innerHTML = '检测到非法调试,请关闭后刷新重试!'
+          }
+          setInterval(() => {
+            ;(function () {
+              return false
+            }
+              ['constructor']('debugger')
+              ['call']())
+          }, 50)
+        }
+        try {
+          block()
+        } catch (err) {}
+      })()
 
       axios
         .get(
